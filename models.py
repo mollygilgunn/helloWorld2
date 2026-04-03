eb9df511cc856f39c3af6507005706c1c82fa274
mollygilgunn <mgilgunn@terpmail.umd.edu>
2026-04-03 09:45:33 -0400

    added user class and usermixin import

--- models.py
+++ models.py
@@ -1,6 +1,5 @@
-import email
-
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 db = SQLAlchemy()
 
 class Student(db.Model):
@@ -42,3 +41,28 @@ class Major(db.Model):
     def __repr__(self):
         return f"{self.major}"
 
+class User(UserMixin, db.Model):
+    __tablename__ = "user"
+
+    user_id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True)
+    first_name = db.Column(db.String(30))
+    last_name = db.Column(db.String(50))
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    role = db.Column(db.String(20))
+
+    def __init__(self, username, first_name, last_name, email, password, role='PUBLIC'):
+        self.username = username
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+        self.role = role
+
+    # Function for flask_login manager to provider a user ID to know who is logged in
+    def get_id(self):
+        return(self.user_id)
+
+    def __repr__(self):
+        return f"{self.first_name} {self.last_name} ({self.username})"
